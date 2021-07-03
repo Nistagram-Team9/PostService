84e67b397196493b8de815834cd24f9e565be158
LeonaNedeljkovic <40694205+LeonaNedeljkovic@users.noreply.github.com>
2021-07-03 15:50:00 +0200

    16# 17# 18# 19# post service (#20)

--- post-service/src/main/java/devops/tim9/postservice/service/PostService.java
+++ post-service/src/main/java/devops/tim9/postservice/service/PostService.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import devops.tim9.postservice.config.domain.AcceptAgentEvent;
+import devops.tim9.postservice.config.domain.DisableUserEvent;
 import devops.tim9.postservice.exception.ImageStorageException;
 import devops.tim9.postservice.model.Comment;
 import devops.tim9.postservice.model.Post;
 import devops.tim9.postservice.model.User;
+import devops.tim9.postservice.producer.AdminEventsProducer;
 import devops.tim9.postservice.repository.CommentRepository;
 import devops.tim9.postservice.repository.PostRepository;
 import devops.tim9.postservice.repository.UserRepository;
@@ -23,6 +29,9 @@ public class PostService {
 	private final PostRepository postRepository;
 	private final ImageStorageService imageStorageService;
 	private final CommentRepository commentRepository;
+	
+	@Autowired
+	AdminEventsProducer adminEventsProducer;
 
 	public PostService(UserRepository userRepository, PostRepository postRepository,
 			ImageStorageService imageStorageService, CommentRepository commentRepository) {
@@ -157,6 +166,47 @@ public class PostService {
 		}
 		return new ArrayList<>();	
 	}
+	
+	public List<Post> getReportedPosts(){
+		return postRepository.findByReported();
+	}
+	
+	public void deletePost(Integer id) {
+		postRepository.deleteById(id);
+	}
+	
+	public List<User> getAgentRequests(){
+		return userRepository.findByIsAccepted(false);
+	}
+	
+	public void disableAccount(String username) {
+		DisableUserEvent disableUserEvent = new DisableUserEvent(null, username);
+		try {
+			adminEventsProducer.sendDisableUserEvent(disableUserEvent);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void acceptAgentRequest(String username) {
+		User user = userRepository.findByUsername(username);
+		if(user!=null) {
+			user.setIsAccepted(true);
+			userRepository.save(user);
+			AcceptAgentEvent acceptAgentEvent = new AcceptAgentEvent(null,username);
+			try {
+				adminEventsProducer.sendAcceptAgentEvent(acceptAgentEvent);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	
+	
 	
 
 }
